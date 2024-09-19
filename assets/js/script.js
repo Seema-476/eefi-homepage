238407aaafcbbd381ed2f6ef4e88cc30c76b3d29
Seema-476 <simmybishnoi29085@gmail.com>
2024-09-19 12:19:03 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function open_nav() {
+    document.getElementById("nav_list").classList.toggle("start-0");
+    document.querySelector(".menu_icon").classList.toggle("cross");
+    document.body.classList.toggle("overflow_hidden");
+}
