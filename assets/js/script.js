ab38d947226e583669dbbc851d30b2d79e29ba90
Seema-476 <simmybishnoi29085@gmail.com>
2024-09-19 22:15:05 +0530

    add elip

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ function open_nav() {
     document.querySelector(".menu_icon").classList.toggle("cross");
     document.body.classList.toggle("overflow_hidden");
 }
+// scroll top btn 
+const topBtn = document.querySelector(".topBtn")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show")
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
